f2b7fe6667168d34768b03e5617e64e1d29ad299
claytod5 <claytod5@protonmail.com>
2021-09-01 10:15:56 -0400

    Add progress bar to install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,69 @@
 #!/bin/bash
 
+# Progress bar
+# progreSh 10
+progreSh() {
+    LR='\033[1;31m'
+    LG='\033[1;32m'
+    LY='\033[1;33m'
+    LC='\033[1;36m'
+    LW='\033[1;37m'
+    NC='\033[0m'
+    if [ "${1}" = "0" ]; then TME=$(date +"%s"); fi
+    SEC=`printf "%04d\n" $(($(date +"%s")-${TME}))`; SEC="$SEC sec"
+    PRC=`printf "%.0f" ${1}`
+    SHW=`printf "%3d\n" ${PRC}`
+    LNE=`printf "%.0f" $((${PRC}/2))`
+    LRR=`printf "%.0f" $((${PRC}/2-12))`; if [ ${LRR} -le 0 ]; then LRR=0; fi;
+    LYY=`printf "%.0f" $((${PRC}/2-24))`; if [ ${LYY} -le 0 ]; then LYY=0; fi;
+    LCC=`printf "%.0f" $((${PRC}/2-36))`; if [ ${LCC} -le 0 ]; then LCC=0; fi;
+    LGG=`printf "%.0f" $((${PRC}/2-48))`; if [ ${LGG} -le 0 ]; then LGG=0; fi;
+    LRR_=""
+    LYY_=""
+    LCC_=""
+    LGG_=""
+    for ((i=1;i<=13;i++))
+    do
+    	DOTS=""; for ((ii=${i};ii<13;ii++)); do DOTS="${DOTS}."; done
+    	if [ ${i} -le ${LNE} ]; then LRR_="${LRR_}#"; else LRR_="${LRR_}."; fi
+    	echo -ne "  ${LW}${SEC}  ${LR}${LRR_}${DOTS}${LY}............${LC}............${LG}............ ${SHW}%${NC}\r"
+    	if [ ${LNE} -ge 1 ]; then sleep .05; fi
+    done
+    for ((i=14;i<=25;i++))
+    do
+    	DOTS=""; for ((ii=${i};ii<25;ii++)); do DOTS="${DOTS}."; done
+    	if [ ${i} -le ${LNE} ]; then LYY_="${LYY_}#"; else LYY_="${LYY_}."; fi
+    	echo -ne "  ${LW}${SEC}  ${LR}${LRR_}${LY}${LYY_}${DOTS}${LC}............${LG}............ ${SHW}%${NC}\r"
+    	if [ ${LNE} -ge 14 ]; then sleep .05; fi
+    done
+    for ((i=26;i<=37;i++))
+    do
+    	DOTS=""; for ((ii=${i};ii<37;ii++)); do DOTS="${DOTS}."; done
+    	if [ ${i} -le ${LNE} ]; then LCC_="${LCC_}#"; else LCC_="${LCC_}."; fi
+    	echo -ne "  ${LW}${SEC}  ${LR}${LRR_}${LY}${LYY_}${LC}${LCC_}${DOTS}${LG}............ ${SHW}%${NC}\r"
+    	if [ ${LNE} -ge 26 ]; then sleep .05; fi
+    done
+    for ((i=38;i<=49;i++))
+    do
+    	DOTS=""; for ((ii=${i};ii<49;ii++)); do DOTS="${DOTS}."; done
+    	if [ ${i} -le ${LNE} ]; then LGG_="${LGG_}#"; else LGG_="${LGG_}."; fi
+    	echo -ne "  ${LW}${SEC}  ${LR}${LRR_}${LY}${LYY_}${LC}${LCC_}${LG}${LGG_}${DOTS} ${SHW}%${NC}\r"
+    	if [ ${LNE} -ge 38 ]; then sleep .05; fi
+    done
+}
+
 font() {
   echo "Installing font: JetBrains Nerd Font"
+  if [[ -e "$HOME/.fonts/"]]; then
+    echo "Fonts directory found"
+  else
+    echo "Creating fonts directory"
+    mkdir $HOME/.fonts/
+  fi
+  cd $HOME/.fonts/
+  wget2 --progress bar https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/JetBrainsMono.zip
+  unzip ./JetBrainsMono.zip
+  cd -
 }
 
 distro_packages() {
@@ -25,11 +87,18 @@ pip() {
   echo "Installing Pipx"
 }
 
+poetry() {
+  echo "Installing poetry"
+
+  echo "Finished installing poetry"
+}
+
 pipx_programs() {
   echo "Installing black"
   echo "Installing isort"
   echo "Installing flake8"
   echo "Installing cookiecutter"
+  echo "Installing ranger"
 }
 
 python() {
@@ -37,6 +106,7 @@ python() {
   pyenv()
   pip()
   pipx_programs()
+  poetry()
   echo "Python setup complete"
 }
 
@@ -88,6 +158,7 @@ build_neovim() {
 
 configs() {
   echo "Setting up configs"
+  stow
 
   echo "Finished configuration"
 }
