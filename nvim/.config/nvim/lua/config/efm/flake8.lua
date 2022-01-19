16331034c5474ac7e17af431a726f11bbeaa71a3
claytod5 <claytod5@protonmail.com>
2022-01-18 22:13:39 -0500

    flake8: ignore 'declare but unused' error

--- nvim/.config/nvim/lua/config/efm/flake8.lua
+++ nvim/.config/nvim/lua/config/efm/flake8.lua
@@ -1,5 +1,5 @@
 return {
-    lintCommand = "flake8 --extend-ignore=F401 --max-line-length 88 --format '%(path)s:%(row)d:%(col)d: %(code)s %(code)s %(text)s' --stdin-display-name ${INPUT} -",
+    lintCommand = "flake8 --extend-ignore=F401,F841 --max-line-length 88 --format '%(path)s:%(row)d:%(col)d: %(code)s %(code)s %(text)s' --stdin-display-name ${INPUT} -",
     lintStdin = true,
     lintIgnoreExitCode = true,
     lintFormats = {"%f:%l:%c: %t%n%n%n %m"},
