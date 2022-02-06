8592073674f3113405390b81564edc13d6679b8b
claytod5 <claytod5@protonmail.com>
2022-02-05 23:39:25 -0500

    create custom terminal for python runs

--- nvim/.config/nvim/ftplugin/python.lua
+++ nvim/.config/nvim/ftplugin/python.lua
@@ -7,11 +7,16 @@
 -- vim.opt_local.shiftwidth = 4
 -- vim.opt_local.textwidth = 88
 
--- vim.cmd("command! Run :lua require('utils').run_python()")
-
 vim.opt_local.expandtab = true
 
--- vim.opt_local.cc = vim.opt_local.cc + 1  -- set column line one more than textwidth
+-- ifmain block
+vim.cmd[[ :iabbrev ifmain if __name__ == "__main__":<CR> ]]
 
 -- File runner
 vim.api.nvim_buf_set_keymap(0, 'n', '<F8>', '<cmd>lua require("utils").run_python()<CR>', { noremap = true })
+
+-- Test runner
+-- vim.api.nvim_buf_set_keymap(0, 'n', '<F4>', '<cmd>lua require("utils").run_python_tests()<CR>', { noremap = true })
+
+-- Easy skip test
+vim.api.nvim_buf_set_keymap(0, 'n', '<leader>st', 'O@unittest.skip("skipping")<ESC>', { noremap = true })
--- nvim/.config/nvim/lua/utils/init.lua
+++ nvim/.config/nvim/lua/utils/init.lua
@@ -1,5 +1,6 @@
 local utils = {}
 local lsp_util = require('lspconfig/util')
+local Terminal  = require('toggleterm.terminal').Terminal
 
 function utils.add_keymap(mode, opts, keymaps)
   for _, keymap in ipairs(keymaps) do
@@ -30,7 +31,7 @@ end
 -- Find either venv or global python
 function utils.get_python_path(workspace)
   local path = lsp_util.path
-  -- Find and use virtualenv via poetry in workspace directory.
+  -- Find and use virtualenv in workspace directory.
   local match = vim.fn.glob(path.join(workspace, '.venv'))
   if match ~= '' then
     if vim.g.windows then
@@ -49,8 +50,39 @@ function utils.run_python()
   local root = vim.loop.cwd()
   local file_path = vim.fn.expand("%:p")
   local python_bin = require('utils').get_python_path(root)
-  vim.api.nvim_command(string.format(':vsplit term://%s %s', python_bin, file_path))
-  print(string.format("%s %s", python_bin, file_path))
+  local python_run = Terminal:new({
+    cmd = string.format('%s %s', python_bin, file_path),
+    direction = 'vertical',
+    close_on_exit = false,
+    on_open = function()
+      print(string.format('%s %s', python_bin, file_path))
+    end
+  })
+  python_run:toggle()
+
+end
+
+-- Use current working directory as 'workspace' to find python and run unittests in current file
+-- function utils.run_python_tests()
+--   local root = vim.loop.cwd()
+--   local file_path = vim.fn.expand("%:p")
+--   local python_bin = require('utils').get_python_path(root)
+--   local python_run_tests = Terminal:new({
+--     cmd = string.format('%s -m unittest %s', python_bin, file_path),
+--     direction = 'vertical',
+--     close_on_exit = false,
+--     on_open = function()
+--       print(string.format('%s -m unittest %s', python_bin, file_path))
+--     end
+--   })
+--   python_run_tests:toggle()
+--
+-- end
+
+function utils.python_bin()
+  local root = vim.loop.cwd()
+  local python_bin = require('utils').get_python_path(root)
+  return python_bin
 end
 
 function utils.loaded_plugins()
