b88380250018ecdf8ffcafe35cf2350c04d017ea
BlakeJC94 <blake.c@montu.com.au>
2025-11-16 20:53:37 +1100

    sync: blake@grapefruit 2025-11-16 20:53:37

--- private_dot_config/nvim/init.lua
+++ private_dot_config/nvim/init.lua
@@ -1,5 +1,7 @@
 vim.g.mapleader = " "
 
-dofile("options.lua")
-dofile("plugins.lua")
-dofile("mappings.lua")
+ vim.fn.stdpath("config")
+
+dofile(vim.fn.stdpath("config") .. "/options.lua")
+dofile(vim.fn.stdpath("config") .. "/plugins.lua")
+dofile(vim.fn.stdpath("config") .. "/mappings.lua")
