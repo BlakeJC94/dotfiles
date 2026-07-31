41accd03959ed5c93e96660547de7e1010bbc600
BlakeJC94 <blakejamescook@pm.me>
2026-07-31 16:17:59 +1000

    Update pi-ext and gitconf

--- .pi/agent/extensions/pi-file-snapshots/index.ts
+++
@@ -1,8 +0,0 @@
-/**
- * pi-file-snapshots entry point.
- *
- * pi auto-discovers `index.ts` in an extension subdirectory; this file just
- * re-exports the factory from `src/extension.ts`.
- */
-
-export { default } from "./src/extension.ts";
