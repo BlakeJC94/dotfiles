77c0dceff129c51c267e554f4cc6b9a66fc7a371
BlakeJC94 <blakejamescook@pm.me>
2026-08-24 09:06:13 +1000

    nvim git keybonds

--- .config/nvim/lua/plugins/gitsigns_nvim.lua
+++ .config/nvim/lua/plugins/gitsigns_nvim.lua
@@ -13,22 +13,22 @@ return {
         },
     },
     keys = {
-        { "]c", ":silent Gitsigns next_hunk<CR>" },
-        { "[c", ":silent Gitsigns prev_hunk<CR>" },
+        { "<Leader>cr", ":Gitsigns refresh<CR>" },
+        { "<Leader>cd", ":Gitsigns toggle_deleted<CR>" },
         { "<Leader>cp", ":Gitsigns preview_hunk_inline<CR>" },
         { "<Leader>cP", ":Gitsigns preview_hunk<CR>" },
-        { "<Leader>cd", ":Gitsigns toggle_deleted<CR>" },
+        { "<Leader>cq", ":Gitsigns setqflist<CR>" },
+        { "<Leader>cQ", ":Gitsigns setloclist<CR>" },
+        { "<Leader>cs", ":Gitsigns stage_hunk<CR>", mode = { "n", "v" } },
+        { "<Leader>cu", ":Gitsigns reset_hunk<CR>", mode = { "n", "v" } },
+        { "<Leader>cw", ":Gitsigns toggle_word_diff<CR>" },
         {
             "<Leader>cl",
             function()
                 require("gitsigns").blame_line({ full = true })
             end,
         },
-        { "<Leader>cq", ":Gitsigns setqflist<CR>" },
-        { "<Leader>cQ", ":Gitsigns setloclist<CR>" },
-        { "<Leader>cr", ":Gitsigns refresh<CR>" },
-        { "<Leader>cw", ":Gitsigns toggle_word_diff<CR>" },
-        { "<Leader>cs", ":Gitsigns stage_hunk<CR>", mode = { "n", "v" } },
-        { "<Leader>cu", ":Gitsigns reset_hunk<CR>", mode = { "n", "v" } },
+        { "[c", ":silent Gitsigns prev_hunk<CR>" },
+        { "]c", ":silent Gitsigns next_hunk<CR>" },
     },
 }
